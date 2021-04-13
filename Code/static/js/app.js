1f869adfb61e6a0da9c11580db331b97825b1319
Jeremy Strauch <jtstrauch58@gmail.com>
2021-04-13 05:00:04 -0500

    select dropdown working

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -8,18 +8,51 @@ function unpack(rows, index) {
     });
   };
 
+  // Call updatePlotly() when a change takes place to the DOM
+// d3.selectAll("#selDataset").on("change", buildBar);
 
-// bar chart function
+
+
+// // bar chart function
 // function buildBar () {
+//   // Use D3 to select the dropdown menu
+//   var dropdownMenu = d3.select("#selDataset");
+//   // Assign the value of the dropdown menu option to a variable
+//   var dataset = dropdownMenu.property("value");
+
     d3.json("samples.json").then(function(data) {
-        console.log(Object.keys(data));
-        var names = data.names;
+        var otuIDs = [];
+        var otuLabels = [];
+        var otuSample = [];
+        for (i=0; i<data.samples.length; i++) {
+            otuIDs.push(data.samples[i].id);
+            otuLabels.push(data.samples[i].otu_labels);
+            otuSample.push(data.samples[i].sample_values);
+        };
+
+        var optionOTU = d3.select("#selDataset");
+        otuIDs.forEach(function(person) {
+        var row = optionOTU.append("option");
+        row.text(person);
+        });
+
+        // On change to the DOM, call getData()
+        d3.selectAll("#selDataset").on("change", getData);
+        function getData() {
+          var dropdownMenu = d3.select("#selDataset");
+          // Assign the value of the dropdown menu option to a variable
+          var dataset = dropdownMenu.property("value");
+          console.log(dataset);}
+  // });
+        // var otuIDs = data.map((item, index) => 
+        // data.samples[index].id);
+        // var names = data.names;
         // var stock = data.dataset.dataset_code;
         // var startDate = data.dataset.start_date;
         // var endDate = data.dataset.end_date;
         // var names = unpack(data.names, 0);
         // var closingPrices = unpack(data.dataset.data, 4);
-        console.log(names);
+        // console.log(names);
 });
 // }
 //     function barchart (data) {
